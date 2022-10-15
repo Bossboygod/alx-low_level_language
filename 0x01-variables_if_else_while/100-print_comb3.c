b43a997a5eb0234e646d5df1631db54cd3abecc2
Bossboygod <joshuaogundeyi80@gmail.com>
2022-10-14 23:36:52 -0700

    Three Digits

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,23 +1,23 @@
 #include <stdio.h>
-
 /**
 * main - Print combinations of two digit numbers
 *
 * Return: Always 0 (Success)
 */
+
 int main(void)
 {
   int tens;
   int ones;
 
-  for (tens = 0; tens <= 9; tens++)
+  for(tens = 0; tens <= 9; tens++)
     {
-      for (ones = tens + 1; ones <= 9; ones++)
+      for(ones = tens + 1; ones <= 9; ones++)
 	{
 	  putchar(tens + '0');
 	  putchar(ones + '0');
 
-	  if (tens < 8)
+	  if(tens < 8)
 	    {
 	      putchar(',');
 	      putchar(' ');
@@ -27,4 +27,4 @@ int main(void)
   putchar('\n');
 
   return (0);
-}
+
