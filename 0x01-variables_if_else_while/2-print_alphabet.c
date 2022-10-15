c1dabf19f13d36211dd62e7b4b995249b1517c1e
Bossboygod <joshuaogundeyi80@gmail.com>
2022-10-14 23:45:52 -0700

    Lowercase Alphabet

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,17 +1,18 @@
 #include <stdio.h>
+
 /**
  * main - Entry point
  *
- * Return: Always 0 (Success)
+ * Return: Always 0.
  */
 int main(void)
 {
+	char letter;
 
-	char c;
+	for (letter = 'a'; letter <= 'z'; letter++)
+		putchar(letter);
 
-	for (c = 'a'; c <= 'z'; c++)
-		putchar(c);
-		putchar('\n');
+	putchar('\n');
 
-return (0);
+	return (0);
 }
